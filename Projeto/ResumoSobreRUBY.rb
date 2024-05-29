a76bed9d05471b36d6d61d9439763f81e9238e99
Claiton Caio Rodrigues <102754355+ClaitonCaio@users.noreply.github.com>
2024-05-29 16:22:55 -0300

    Create ResumoSobreRUBY.rb

---
+++ Projeto/ResumoSobreRUBY.rb
@@ -0,0 +1,56 @@
+# Ideias de Jogos que pode ser feito com a Linguagem Ruby: # 
+
+# 3 -- Jogo PONG em Ruby -- #
+
+
+#---------------------------------------------------------------
+# Origem da Linguagem Ruby: 
+
+# Criada no Japão em 1995 por Yukihiro “Matz” Matsumoto, a linguagem de Programação Ruby é uma linguagem dinâmica, open source, inspirada em linguagens como Python, 
+# Perl, Lisp, entre outras, com foco na simplicidade e na produtividade.
+# Muito similar em vários aspectos com o Python, possui uma sintaxe elegante, de leitura natural e de fácil escrita.
+# Multiplataforma, o Ruby possui uma excelente curva de aprendizagem. #
+
+
+#---------------------------------------------------------------
+
+# Caracteristicas da Ruby: 
+
+# - Multiplataforma;
+# - Open source;
+# - Multiparadigma;
+# - Tudo é tratado como objeto assim como no Python;
+# - Flexível, uma vez que permite aos seus utilizadores alterar partes da linguagem, sendo assim, partes essenciais do Ruby podem ser removidas ou redefinidas à vontade;
+# - Utiliza indentação por espaços;
+# - Não necessita de declarações de variáveis;
+# - Possui sistema de threading independente do Sistema Operacional, entre outras.
+
+#----------------------------------------------------------------
+
+# Plataformas que usam Ruby:
+
+# - Soundcloud
+# - Twitch
+# - Twitter
+# - Airbnb
+# - Zendesk
+
+#----------------------------------------------------------------
+
+# Vantagens da Ruby:
+
+# - Compatível com métodos ágeis, como Extreme Programming (XP) e Scrum;
+# - Comunidade ampla e engajada;
+# - Ecossistema rico com várias opções de bibliotecas; 
+# - Fácil desenvolvimento de sites;
+# - Multiplataforma;
+# - Open source.
+
+#----------------------------------------------------------------
+
+# Ruby GEMS:
+
+#É um sistema que ajuda os programadores a gerenciar e compartilhar bibliotecas de código (também conhecidas como "gemas" ou "gems" ) em Ruby. Uma gema é um pacote de
+#software que pode conter código, documentação, ou qualquer coisa que possa ser útil para um projeto. Com o RubyGems, os desenvolvedores podem facilmente instalar, atualizar e
+#compartilhar essas bibliotecas para melhorar a eficiência e a reutilização de código em projetos Ruby. É como uma loja de aplicativos, onde você pode encontrar e baixar funcionalidades
+#prontas para serem usadas em seus programas.
